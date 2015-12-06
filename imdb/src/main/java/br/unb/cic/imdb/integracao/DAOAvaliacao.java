a694b5cb4235336ec8bf1b083b33493668e4284d
iurebrandao <iurebrandao@aluno.unb.br>
2015-12-06 17:44:48 -0200

    DAOAvaliacao

--- imdb/src/main/java/br/unb/cic/imdb/integracao/DAOAvaliacao.java
+++ imdb/src/main/java/br/unb/cic/imdb/integracao/DAOAvaliacao.java
@@ -1,12 +1,14 @@
 package br.unb.cic.imdb.integracao;
 
 import br.unb.cic.imdb.negocio.Avaliacao;
+import br.unb.cic.imdb.negocio.TrabalhoArtistico;
+import br.unb.cic.imdb.negocio.Usuario;
 
 public interface DAOAvaliacao {
 
 	public void salvar(Avaliacao avaliacao);
-	public Avaliacao recuperarPorUsuario();
-	public Avaliacao recuperarPorTrabalhoArtistico();
+	public Avaliacao recuperarPorUsuario(Usuario usuario);
+	public Avaliacao recuperarPorTrabalhoArtistico(TrabalhoArtistico trabalho);
 	
 	
 }
--- imdb/src/main/java/br/unb/cic/imdb/integracao/jpa/DAOAvaliacaoJPA.java
+++ imdb/src/main/java/br/unb/cic/imdb/integracao/jpa/DAOAvaliacaoJPA.java
@@ -1,26 +1,40 @@
 package br.unb.cic.imdb.integracao.jpa;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import br.unb.cic.imdb.integracao.DAOAvaliacao;
 import br.unb.cic.imdb.negocio.Avaliacao;
+import br.unb.cic.imdb.negocio.TrabalhoArtistico;
+import br.unb.cic.imdb.negocio.Usuario;
 
 public class DAOAvaliacaoJPA implements DAOAvaliacao {
-
+	
+	private EntityManager em;
+	
 	@Override
 	public void salvar(Avaliacao avaliacao) {
-		// TODO Auto-generated method stub
-		
+		em = EMFactoryHelper.instance().getFactory().createEntityManager();
+		em.getTransaction().begin();
+		em.persist(avaliacao);
+		em.getTransaction().commit();
 	}
 
 	@Override
-	public Avaliacao recuperarPorUsuario() {
-		// TODO Auto-generated method stub
-		return null;
+	public Avaliacao recuperarPorUsuario(Usuario usuario) {
+		em = EMFactoryHelper.instance().getFactory().createEntityManager();
+		List<Avaliacao> avaliacaoUsuarios = em.createQuery("FROM Usuario WHERE avaliacao = :nomeParam")
+				.setParameter("nomeParam", usuario.getNome()).getResultList();
+		return avaliacaoUsuarios.size() == 1 ? avaliacaoUsuarios.get(0) : null;
 	}
 
 	@Override
-	public Avaliacao recuperarPorTrabalhoArtistico() {
-		// TODO Auto-generated method stub
-		return null;
+	public Avaliacao recuperarPorTrabalhoArtistico(TrabalhoArtistico trabalho) {
+		em = EMFactoryHelper.instance().getFactory().createEntityManager();
+		List<Avaliacao> avaliacaoUsuarios = em.createQuery("FROM TrabalhoArtistico WHERE avaliacao = :tituloParam")
+				.setParameter("nomeParam", trabalho.getTitulo()).getResultList();
+		return avaliacaoUsuarios.size() == 1 ? avaliacaoUsuarios.get(0) : null;
 	}
 
 }
